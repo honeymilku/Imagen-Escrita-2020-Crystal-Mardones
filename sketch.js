c4fc90e3964f633ed04a44818a8ab8160aaca9d0
honeymilku <69780387+honeymilku@users.noreply.github.com>
2020-09-04 15:02:33 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,55 @@
 function setup() {
-  createCanvas(700, 400);
+  createCanvas(400, 400);
   
 }
 
 function draw() {
+  background (137, 238, 209);
+  
+  
+  //pelo
+  fill(247, 255, 125);
+  stroke(0)
+  strokeWeight(0)
+  ellipse(200, 150, 180, 200)
+  rect(120, 100, 160, 220)
 
-  for(let i = 0; i < 20; i++){
-  stroke(random (250), random (250), random (250));
-  strokeWeight(random (2, 20));
-  point(random(width), random(height));
-  }
+  //cuerpazooo xd
+	stroke(0);
+	strokeWeight(0);
+	fill(196, 175, 155);
+  rect(130, 260, 150, 400, 580); //ropa
+
+  stroke(0);
+	fill(250, 230, 202);
+  ellipse(200, 180, 160, 170); //cara
+
+  fill(247, 255, 125);
+  stroke(0)
+  strokeWeight(0)
+  rect(125, 90, 150, 60)
+  
+
+  fill(105, 94, 82);
+	stroke(0);
+	ellipse(170, 180, 10, 20); //ojos
+  ellipse(230, 180, 10, 20); 
+  
+  fill(249, 219, 247);
+  stroke(0);
+  ellipse(150, 210, 50, 50);
+  ellipse(250, 210, 50, 50);
+
+  fill(0)
+  rect(185, 215, 10, 10, 20);
+
+  
+
+  
+
+
+
+  
+ 
+
 }
